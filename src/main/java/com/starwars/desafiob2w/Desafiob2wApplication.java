b105041b60c53824b4028473d71566bcaa50642c
Carlos Henrique <carlos_hdc@hotmail.com>
2021-01-24 17:24:21 -0300

    initial commit

---
+++ src/main/java/com/starwars/desafiob2w/Desafiob2wApplication.java
@@ -0,0 +1,13 @@
+package com.starwars.desafiob2w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafiob2wApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Desafiob2wApplication.class, args);
+    }
+
+}
