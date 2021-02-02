a2bd508921e4ef3ae24292543d74e9223971fbfe
Carlos Henrique <carlos_hdc@hotmail.com>
2021-02-01 21:28:40 -0300

    add sanity check

--- src/test/java/com/starwars/desafiob2w/Desafiob2wApplicationTests.java
+++ src/test/java/com/starwars/desafiob2w/Desafiob2wApplicationTests.java
@@ -1,13 +1,22 @@
 package com.starwars.desafiob2w;
 
+import com.starwars.desafiob2w.controllers.PlanetController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class Desafiob2wApplicationTests {
 
+    @Autowired
+    private PlanetController controller;
+
     @Test
+    //sanity check
     void contextLoads() {
+        assertThat(controller).isNotNull();
     }
 
 }
